a292a6e4b1585f602a390a48f72559b7de4c5d5e
Keeeal <james@keal.com.au>
2020-06-04 11:31:41 +0930

    Update to work with new discord package.

--- quothbot.py
+++ quothbot.py
@@ -1,109 +1,118 @@
-import os, random, asyncio, json, discord
 
-def load_messages(json_path):
-    with open(json_path, 'r') as f:
+import os, asyncio, json, discord
+from random import choice
+
+def load_messages(json_file,
+        bots=[], whitelist=[], blacklist=[],
+        urls=False, tags=False):
+
+    with open(json_file) as f:
         data = json.load(f)
 
     messages = {}
-    users, u_idx = data['meta']['users'], data['meta']['userindex']
+    usernames = {k:v['name'] for k, v in data['meta']['users'].items()}
+    idx = data['meta']['userindex']
 
     for channel_id, channel_data in data['data'].items():
         for message_id, message_data in channel_data.items():
-            u = users[u_idx[message_data['u']]]['name']
-            m = message_data['m']
 
-            if not len(m): # empty
+            # get username
+            u = usernames[idx[message_data['u']]]
+            if len(whitelist) and u not in whitelist:
                 continue
-            if 'http' in m or 'www.' in m: # urls
-                continue
-            if m.startswith('!') or m.startswith('-') or m.startswith(';;'): # bot commands
-                continue
-            if m.startswith('<@') and m.endswith('>'): # lone tags
+            if u in blacklist:
                 continue
 
-            if u in messages:
-                messages[u] += [m]
-            else:
-                messages[u] = [m]
+            # get message
+            if 'm' in message_data:
+                m = message_data['m']
 
-    users_path = json_path.split('.')
-    users_path = '.'.join(users_path[:-1] + ['users'] + users_path[-1:])
+                # filter messages
+                for sign in bots:
+                    if m.startswith(sign):
+                        m = ''
+                if not urls:
+                    if 'http' in m or 'www.' in m:
+                        m = ''
+                if not tags:
+                    if m.startswith('<@') and m.endswith('>'):
+                        m = ''
 
-    if os.path.isfile(users_path):
-        with open(users_path, 'r') as f:
-            keep_users = ''.join(f.readlines()).split(',')
-    else:
-        keep_users = []
+                if not m: continue
+                if u not in messages: messages[u] = []
+                messages[u].append(m)
 
-        for u, m in sorted(messages.items(), key=lambda x: len(x[1]), reverse=True):
-            while True:
-                keep = input('Keep ' + u + '? (' + str(len(m)) + ' messages) [y/n] ')
+    return messages
 
-                if keep == 'y':
-                    keep_users.append(u)
-                    break
-                elif keep == 'n':
-                    break
+def embed(channel, user, message):
+    for member in channel.guild.members:
+        avatar = member.default_avatar_url
+        if member.name == user:
+            if member.nick: user = member.nick
+            avatar = member.avatar_url
+            break
 
-        with open(users_path, 'w') as f:
-            for user in keep_users:
-                f.write(user + ',')
-
-    return {k:v for k, v in messages.items() if k in keep_users}
-
-def get_embed(channel, messages):
-    members = channel.server.members
-    user = random.choice(list(messages.keys()))
-    member = discord.utils.find(lambda m: m.name == user, members)
-
-    if member.nick:
-        name = member.nick
-    else:
-        name = user
-
-    if member.avatar_url:
-        avatar_url = member.avatar_url
-    else:
-        avatar_url = member.default_avatar_url
-
-    message = random.choice(messages[user])
     embed = discord.Embed(description=message)
-    embed.set_author(name=name, icon_url=avatar_url)
-
+    embed.set_author(name=user, icon_url=avatar)
     return embed
 
-def main(json_file):
-    print('\nLoading', json_file, '...')
-    messages = load_messages(json_file)
+def main(json_file, config_file=None):
+
+    # load config file or create it
+    if not config_file:
+        config_file = 'config.txt'
+    if os.path.isfile(config_file):
+        with open(config_file) as f:
+            config = json.load(f)
+    else:
+        config = {
+            'bot signs': ['!', '-', ';;', '>>'],
+            'user whitelist': [],
+            'user blacklist': [],
+            'allow urls': False,
+            'allow lone tags': False,
+        }
+        with open(config_file, 'w') as f:
+            json.dump(config, f, indent=2)
+
+    # load data
+    print('\nLoading {}...'.format(json_file))
+    messages = load_messages(json_file,
+        bots=config['bot signs'],
+        whitelist=config['user whitelist'],
+        blacklist=config['user blacklist'],
+        urls=config['allow urls'],
+        tags=config['allow lone tags'],
+    )
+    n_messages = [(len(m), u) for u, m in messages.items()]
+    for n, u in sorted(n_messages, reverse=True):
+        print('Loaded {}: {} messages'.format(u, n))
+
+    # create client
     client = discord.Client()
 
     @client.event
     async def on_ready():
-        print('\nLogged in as', client.user.name, '\n')
-
-        for u, m in sorted(messages.items(), key=lambda x: len(x[1]), reverse=True):
-            print(u, '-', len(m), 'messages')
+        print('Logged in as {}'.format(client.user.name))
 
     @client.event
     async def on_reaction_add(reaction, user):
         channel = reaction.message.channel
-
         if reaction.emoji == '🐦':
-            await client.send_message(channel, embed=get_embed(channel, messages))
+            user = choice(list(messages.keys()))
+            message = choice(messages[user])
+            await channel.send(embed=embed(channel, user, message))
 
+    # read auth token
     with open('token.txt') as f:
         token = f.readline()[:-1]
 
-    while True:
-        try:
-            client.run(token)
-        except KeyboardInterrupt:
-            raise
-        except:
-            pass
+    # start bot
+    client.run(token)
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('json_file')
+    parser.add_argument('--config_file', '-c')
     main(**vars(parser.parse_args()))
